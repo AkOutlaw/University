e1e445ddbbb0d7f9c8d53a58e578dab39c5bf3dd
AkOutlaw <outlaw.mwdn@gmail.com>
2019-11-03 01:23:42 +0200

    Adding Controller and Main file
    Adding Controller and Main file

---
+++ Controller.py
@@ -0,0 +1,7 @@
+class Controller:
+    def __init__(self):
+        return 1
+
+    def PrintBD(listOfHumans):
+        print(listOfHumans)
+        return 1
---
+++ Main.py
@@ -0,0 +1,5 @@
+import Student
+import Teacher
+import Controller
+
+print('Here will be my course application')

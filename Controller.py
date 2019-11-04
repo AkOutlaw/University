c504b6b050d861376241ef58bcbd8c7c50d8263b
AkOutlaw <outlaw.mwdn@gmail.com>
2019-11-04 09:35:59 +0200

    Application logic update according to ssd
    Add getSalaryOrScholarship method to Controller

--- Controller.py
+++ Controller.py
@@ -6,37 +6,62 @@ class Controller:
 
     def getListOfMunans(mass):
         listOfMunans = []
-        listOfStudents = []
-        listOfTeachers = []
+        # listOfStudents = []
+        # listOfTeachers = []
 
         for row in mass:
             temp = []
+            isStudent = 0
+            #print(row[3])
+            if 'salary' in row[3]:
+                isStudent = 0
+            else:
+                isStudent = 1
+            #print(isStudent)
             for elem in row:
-                #print(elem)
                 elem = (elem[elem.find("=") + 1 : ])
                 temp.append(elem)
-            #print(temp)
-            student = Humans.Student(*temp)
-            listOfMunans.append(student)
+            if isStudent == 1:
+                student = Humans.Student(*temp)
+                listOfMunans.append(student)
+                #listOfStudents.append(student)
+            elif isStudent == 0:
+                teacher = Humans.Teacher(*temp)
+                listOfMunans.append(teacher)
+                #listOfTeachers.append(teacher)
+
         return listOfMunans
 
+    def getSalaryOrScholarship(listOfHumans):
+        for obj in listOfHumans: # get class object one by one
+            #print(type(obj))
+            if hasattr(obj, 'scholarship'):
+                return obj.scholarship
+            elif hasattr(obj, 'salary'):
+                return obj.salary
+
+# Process of sorting by Salary
     def sortBySalaryHighToLow(listOfHumans):
-        listOfHumansOrdered = sorted(listOfHumans, key=lambda x: int(x.scholarship), reverse=True)
+        #listOfHumansOrdered = sorted(listOfHumans,  key=lambda x: int(x.scholarship), reverse=True)  # sorting from high to low
+        listOfHumansOrdered = sorted(listOfHumans, key=lambda x: int(x.scholarship) if int(x.scholarship) else int(x.salary), reverse=True)  # sorting from high to low
         return listOfHumansOrdered
 
-
+# Process of printing data using formation
     def printBD(listOfHumans):
-        for obj in listOfHumans:
-            print('|First name - %6s;  |  Last name - %10s;  |   Date Of Birth - %10s;  |  scholarship - %5s;  |  faculty - %4s;  |  courseofstudy - %9s;|'
-                  % (obj.firstName, obj.surName, obj.dataOfBirth, obj.scholarship, obj.faculty, obj.courseOfStudy))
-
-
+        for obj in listOfHumans: # get class object one by one
+            #print(type(obj))
+            if hasattr(obj, 'courseOfStudy'):
+                print('|First name - %6s;  |  Last name - %10s;  |   Date Of Birth - %10s;  |  scholarship- %5s;  |  faculty - %4s;  |  courseofstudy - %9s;|' # get fields from object
+                      % (obj.firstName, obj.surName, obj.dataOfBirth, obj.scholarship, obj.faculty, obj.courseOfStudy))
+            if hasattr(obj, 'experience'):
+                print('|First name - %6s;  |  Last name - %10s;  |   Date Of Birth - %10s;  |  Salary - %5s;  |  faculty - %4s;  |  expirience - %9s;|' # get fields from object
+                      % (obj.firstName, obj.surName, obj.dataOfBirth, obj.salary, obj.faculty, obj.experience))
+# Process of reading data from file; return as massive
     def readMass(path):
         file = open(path)
         mass = []
         for line in file:
-            row = line.strip().split(' ')
-            mass.append(row)
-
+            row = line.strip().split(' ') # string concatination
+            mass.append(row) # adding per row to massive
         file.close()
         return mass
--- Main.py
+++ Main.py
@@ -1,25 +1,11 @@
-import Student
-import Teacher
-import Humans
 import Controller
 
-print('Here will be my course application')
-teacher = Humans.Teacher('Ben', 'Jones', '12.08.1981', 8900, 'KIT', 7)
-student = Humans.Student('Ben', 'Davis', '12.08.2001', 900, 'KIT', 2)
-#print(teacher.surName, student.surName)
+print('Here will be my course application!!!')
 mass = Controller.Controller.readMass(r'C:\Users\Outlaw\PycharmProjects\University\input.txt')
-print('')
-#Controller.Controller.printBD(Controller.Controller.getListOfMunans(mass))
+print('-----------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+print('Here is the data from the file:')
+Controller.Controller.printBD(Controller.Controller.getListOfMunans(mass))
+print('-----------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+print('Here is the sorted data from the file:')
+print('-----------------------------------------------------------------------------------------------------------------------------------------------------------------------')
 Controller.Controller.printBD(Controller.Controller.sortBySalaryHighToLow(Controller.Controller.getListOfMunans(mass)))
-
-
-#Controller.Controller.printMass(mass)
-# print(Controller.Controller.getSumByColumn(mass))
-# #print(type(mass))
-# Controller.printList(Controller.getSumByColumn(mass))
-# #res = change(mass)
-# print()
-# print('Number of coulumn and max Value are:: ', Controller.maxElem(Controller.getSumByColumn(mass)))
-
-#printMass(mass,N)
-#print(getSumByRow(mass))
